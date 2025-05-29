382313849d8bb5cee3a0d2066ed0e7efd92ed728
doomowenok <doomowenok@icloud.com>
2025-05-29 18:10:41 +0300

    Add move speed component.

---
+++ Game/Assets/Source/Gameplay/Core/Move/MoveSpeed.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+
+namespace Gameplay.Core
+{
+    public struct MoveSpeed : IComponentData
+    {
+        public float Value;
+    }
+}
---
+++ Game/Assets/Source/Gameplay/Core/Move/MoveSpeedAuthoring.cs
@@ -0,0 +1,22 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace Gameplay.Core
+{
+    public class MoveSpeedAuthoring : MonoBehaviour
+    {
+        public float Value;
+        
+        private class MoveSpeedAuthoringBaker : Baker<MoveSpeedAuthoring>
+        {
+            public override void Bake(MoveSpeedAuthoring authoring)
+            {
+                Entity entity = GetEntity(TransformUsageFlags.Dynamic);
+                AddComponent<MoveSpeed>(entity, new MoveSpeed
+                {
+                    Value = authoring.Value
+                });
+            }
+        }
+    }
+}
