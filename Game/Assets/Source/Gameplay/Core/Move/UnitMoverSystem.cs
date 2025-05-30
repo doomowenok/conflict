206a9566badb3e63d814e7106f111591f7dc0059
doomowenok <doomowenok@icloud.com>
2025-05-30 11:09:45 +0300

    Implement simple move system.

---
+++ Game/Assets/Source/Gameplay/Core/Move/UnitMoverSystem.cs
@@ -0,0 +1,37 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace Gameplay.Core
+{
+    public partial struct UnitMoverSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            float deltaTime = SystemAPI.Time.DeltaTime;
+
+            foreach (var (localTransform,
+                         moveSpeed)
+                     in SystemAPI.Query<
+                         RefRW<LocalTransform>,
+                         RefRO<MoveSpeed>>())
+            {
+                localTransform.ValueRW.Position =
+                    localTransform.ValueRO.Position +
+                    new float3(moveSpeed.ValueRO.Value, 0.0f, 0.0f) * deltaTime;
+            }
+        }
+
+        [BurstCompile]
+        public void OnDestroy(ref SystemState state)
+        {
+        }
+    }
+}
